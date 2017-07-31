455db4b79c6a7a2a724bbf35d1a689c542fd418d
Gana <ganasn@gmail.com>
2017-07-31 05:10:15 -0700

    Exercise 44b - explicit override for inheritance - 20170731-0510

---
+++ ex44b.py
@@ -0,0 +1,19 @@
+#Exercise 44 - Inheritance v Composition
+#Overriding explicitly
+
+class Parent(object):
+    
+    def override(self):
+        print 'Parent override()'
+
+class Child(Parent):
+    def override(self):
+        print 'Child override()'
+
+
+
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
