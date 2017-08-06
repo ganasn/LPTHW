29e5c0db179800a697e1c71a54ebfb80b34a4cb5
Gana <ganasn@gmail.com>
2017-08-06 07:49:11 -0700

    Ex49-usage-WIP-20170806-0749

---
+++ ex49-sd.py
@@ -0,0 +1,11 @@
+#Ex49 - Using Matching Sentences classes
+
+from ex49 import Sentence
+#from sys import argv
+
+#script, word_list[] = argv
+
+#sentence = Sentence()
+word_list = raw_input('Enter your sentence to parse>')
+sentence = Sentence(word_list)
+#sentence.peek(word_list)
